5ba030ce2e5b37e1d6f0198b331138f705edab30
duyvan <duyvanlee2001@gmail.com>
2026-06-04 23:37:17 +0700

    up

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -6,7 +6,9 @@ import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), basicSsl()],
+  plugins: [react(), tailwindcss(), 
+    // basicSsl()
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
