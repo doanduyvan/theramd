e3d287b798d6d8ac0b88b80b473ceea0847db7c9
duyvan <duyvanlee2001@gmail.com>
2026-06-04 23:40:04 +0700

    up

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -15,7 +15,11 @@ export default defineConfig({
     },
   },
   server: {
-    // host: "0.0.0.0",
+    host: "0.0.0.0",
+      allowedHosts: [
+      "odinn.site",
+      "www.odinn.site",
+    ],
     // https: true,
   },
 });
